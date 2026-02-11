665d6dc48254d988087c9d3b927f3a9513e75cbe
Juan Garcia <juan.garciad.col@upb.edu.co>
2026-02-11 13:06:54 -0500

    feat: agregar endpoint PUT para actualizar estudiante

--- routes/estudianteRoutes.js
+++ routes/estudianteRoutes.js
@@ -4,5 +4,6 @@ const estudianteController = require("../controllers/estudianteController");
 
 router.get("/", estudianteController.listarEstudiantes);
 router.post("/", estudianteController.registrarEstudiante);
+router.put("/:id", estudianteController.actualizarEstudiante);
 
 module.exports = router;
